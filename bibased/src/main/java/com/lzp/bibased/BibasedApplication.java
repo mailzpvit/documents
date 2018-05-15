9d420edd9a4d4cd8fc11e4e0a673eecf5a36d119
SOFEWARELZP <243510418@qq.com>
2018-05-15 08:18:00 +0800

    摘要

---
+++ bibased/src/main/java/com/lzp/bibased/BibasedApplication.java
@@ -0,0 +1,12 @@
+package com.lzp.bibased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibasedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibasedApplication.class, args);
+    }
+}
---
+++ bibased/src/test/java/com/lzp/bibased/BibasedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lzp.bibased;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BibasedApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
